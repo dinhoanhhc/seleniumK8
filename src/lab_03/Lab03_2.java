9a249abf42c7906085c998c2e547d0df1e4cbe96
oanhdt <dinhoanhhc@gmail.com>
2022-06-01 00:50:49 +0900

    lab03

--- src/lab_03/Lab03_2.java
+++ src/lab_03/Lab03_2.java
@@ -1,2 +1,35 @@
-package lab_03;public class Lab03_2 {
+package lab_03;
+
+public class Lab03_2 {
+
+    public static void main(String[] args) {
+        int[] myNum = {24,4,3,2,7,14,22,9,12,1};
+        int max = myNum[0];
+        int min = myNum[0];
+        System.out.println("Array is: ");
+
+        for (int a = 0; a < myNum.length; a++) {
+            System.out.print(" " + myNum[a]);
+
+        }
+
+        for (int i = 0; i < myNum.length; i++) {
+            if (myNum[i] > max){
+                max = myNum[i];
+            }
+        }
+
+        for (int j = 0; j < myNum.length; j++) {
+            if (myNum[j] < min){
+                min = myNum[j];
+            }
+
+        }
+
+        System.out.println();
+        System.out.println("max is " + max);
+        System.out.println("min is " + min);
+
+
+    }
 }
--- src/lab_03/Lab03_3.java
+++ src/lab_03/Lab03_3.java
@@ -1,2 +1,32 @@
-package lab_03;public class Lab03_3 {
+package lab_03;
+
+public class Lab03_3 {
+
+    public static void main(String[] args) {
+        int[] myArray = {24,4,3,2,7,14,22,9,12,1};
+
+        System.out.println("Array is: ");
+
+        for (int a = 0; a < myArray.length; a++) {
+            System.out.print(" " + myArray[a]);
+        }
+
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = 0; j < myArray.length; j++) {
+                if (myArray[j] > myArray[i]){
+                    int var = myArray[j];
+                    myArray[j] = myArray[i];
+                    myArray[i] = var;
+                }
+            }
+        }
+
+        System.out.println();
+        System.out.println("Sort: ");
+
+        for (int b = 0; b < myArray.length; b++) {
+            System.out.print(" " + myArray[b]);
+        }
+
+    }
 }
